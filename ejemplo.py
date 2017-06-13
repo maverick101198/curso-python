22f9a3c6147954e84b956c3863990651023de6a1
Udem <udem@fedora.mshome.net>
2017-06-12 18:00:57 -0600

    clase 4

---
+++ ejemplo.py
@@ -0,0 +1,17 @@
+#Dado el texto 'esto es una prueba' imprimir una cadena formateada de la sig. manera
+
+#'esto es una prueba <nombre>'
+#-imprimir cuantas letras 'e' tiene la cadena 
+#-capitalizar cadena
+#-imprimir longitud de cadena 
+#-reemplazar en la cadena de la latra 'o' por 0
+
+dato = (input ("Ingrese el texto: "))
+texto = dato
+cuenta = 0
+for carac in texto:
+    if carac == 'e':
+        cuenta += 1
+print ( "El", dato, "tiene ", cuenta, "letra e")
+print ("Capitalización: ", texto.upper())
+print ("La longitud de la cadena es de: ", len(dato))
