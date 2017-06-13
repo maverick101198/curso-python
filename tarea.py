015bb2f1b5de766998216e28ffba7b179b615174
Udem <udem@fedora.mshome.net>
2017-06-13 14:11:09 -0600

    tarea

---
+++ tarea.py
@@ -0,0 +1,5 @@
+Nombre = input("Digita el nombre: ")
+Edad = int(input("Digite su edad: "))
+dato = 100 - Edad
+cumplira = 2017 + dato
+print ("<<",Nombre,">>, cumplira 100 años en: ", str(cumplira))
