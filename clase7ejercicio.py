48d6efa7532cfbbc4fd5aaa269409797d3834b42
Udem <udem@fedora.mshome.net>
2017-06-20 17:13:48 -0600

    clase 7 ejercicio 1

---
+++ clase7ejercicio.py
@@ -0,0 +1,18 @@
+import argparse
+
+if __name__ == '__main__':
+	parser= argparse.ArgumentParser('val')
+	parser.add_argument('Nombre')
+	parser.add_argument('Edad')
+	parser.add_argument('Clase')
+
+	args = parser.parse_args()
+
+	datos = {
+	'Nombre': args.Nombre,
+	'Edad': args.Edad,
+	'Clase': args.Clase
+	}
+
+	for llave,valor in datos.iteritems():
+	    print llave,valor
