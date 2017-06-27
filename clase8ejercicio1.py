3d10862fc30b96962ae9110ed36b6c3a815cfbca
Udem <udem@fedora.mshome.net>
2017-06-27 16:17:32 -0600

    clase 8 ejercicio 1

--- clase8ejercicio1.py
+++ clase8ejercicio1.py
@@ -1,39 +1,59 @@
-import random
 
+import random
+mensaje= "el {0} de {1} de edad, es de la ciudad de {2} y cursa {3} en la universidad"
 NOMBRES = [
     'Ana',
-    'Rogelio',
-    'Alvaro',
-    'Heyling',
-    'Tom',
-    'Maverick',
-    'Roberto',
-    'Josue',
-    'Giselle',
+    'Pedro',
+    'Pablo',
+    'Ernesto',
+    'Ariel',
+    'Carlos',
+    'Luis',
+    'Oscar',
+    'Alicia',
     'Maria',
-    'Dolores'
+    'Brenda'
 ]
 
 CIUDADES = [
-    'Nicaragua',
-    'Alemania',
-    'Raccoon city',
-    'Silent Hill',
-    'Zurbaran',
-    'Madrid'
+    'Managua',
+    'Masaya',
+    'Matagalpa',
+    'Chinandega',
+    'Somoto',
+    'Rivas'
 ]
-
+import random
 
 def generar_diccionario_estudiantes():
     estudiantes = {}
 
     for nombre in NOMBRES:
-        estudiantes= {
-            'Nombre': random.choice(NOMBRES),
-            'Edad': random.randrange(16, 30),
-            'Year en universidad': random.randrange(1, 5),
-            'Cuidad': random.choice(CIUDADES)
+        estudiantes[nombre] = {
+            'edad': random.randrange(16, 30),
+            'anio': random.randrange(1, 5),
+            'ciudad': random.choice(CIUDADES)
         }
-        return estudiantes
+    return estudiantes
 
-print generar_diccionario_estudiantes()
+if __name__ == '__main__':
+    diccio = generar_diccionario_estudiantes()
+ 
+    for llave,valor in diccio.iteritems() :
+        print llave,valor
+
+    for llave,valor in diccio.iteritems():
+        print mensaje.format(llave,valor['edad'],valor['ciudad'],valor['anio'])
+    print 'estudiantes de managua'
+    for llave,valor in diccio.iteritems():
+        if valor['ciudad'] == 'Managua':
+            print mensaje.format(llave,valor['edad'],valor['ciudad'],valor['anio'])
+    print 'estudiantes de masaya que cursan 1er anio'
+    for llave,valor in diccio.iteritems():
+        if valor['ciudad'] == 'Masaya' and valor['anio'] == 1:
+            print mensaje.format(llave, valor['edad'], valor['ciudad'], valor['anio'])
+    print 'estudiantes menores de 21'
+    for llave,valor in diccio.iteritems():
+        if valor['edad']<21:
+            msj = 'el {0} de {1}, es de la ciudad de {2} y cursa {3} de la universidad'
+print msj.format(llave,valor['edad'],valor['ciudad'],valor['anio'])
